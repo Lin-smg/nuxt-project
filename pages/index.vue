b5c61a97a8c85e235d9a2f21f14510056831a02b
yelinIT <yelinphyocu@gmail.com>
2020-12-24 17:21:03 +0630

    init1121

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,38 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-project
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
+      <app-logo />
+      <h1 class="title">nuxt-project</h1>
+      <h2 class="subtitle">Nuxt.js project</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey">GitHub</a>
+          class="button--grey"
+          >GitHub</a
+        >
+      </div>
+      <div>
+        <b-button>Button</b-button>
+        <b-button variant="danger">Button</b-button>
+        <b-button variant="success">Button</b-button>
+        <b-button variant="outline-primary">Button</b-button>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from "~/components/AppLogo.vue";
 
 export default {
   components: {
-    AppLogo
-  }
-}
+    AppLogo,
+  },
+};
 </script>
 
 <style>
@@ -42,7 +45,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
